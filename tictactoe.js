259216870365263f43090c758c591c4250a14bbe
trevcyb <trevontewari@gmail.com>
2020-11-30 21:31:22 -0400

    More things to fix

--- tictactoe.js
+++ tictactoe.js
@@ -1,62 +1,62 @@
-const gameBoard = () => {
-    let board = ["", "", "", "", "", "", "", "", ""];
-    const boardContainer = document.getElementById("gameContainer");
-    //    board = ["X","O","X","O","X","O","X","O","X"];
-    const createBoard = () => {
-        for (let i = 0; i < board.length; i++) {
+const gameBoard = {
+    board: ["", "", "", "", "", "", "", "", ""],
+    get createBoard() {
+        boardContainer = document.getElementById("gameContainer");
+        for (let i = 0; i < this.board.length; i++) {
             const boardPlace = document.createElement("div");
-            boardPlace.innerHTML = board[i];
+            boardPlace.innerHTML = this.board[i];
             boardPlace.setAttribute = ("placePosition", i);
             boardContainer.appendChild(boardPlace).className = "grid-item";
-        };
-        return { createBoard };
-    };
-};
+        }
+    }
+}
 
-const player = (name) => {
-    const getName = () => name;
-    const firstturn = () => {
-        if ((Math.floor(Math.random(1, 10)) % 2) === 0) {
-            return true;
-        } else {
-            return false;
+function player(name) {
+    return {
+        name,
+        firstturn() {
+            if ((Math.floor(Math.random(1, 10)) % 2) === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        symbolSelection() {
+            if (document.getElementById("p1").checked) {
+                selection = "O";
+                p2selection = "X";
+            } else {
+                selection = "X";
+                p2selection = "O";
+            }
+            return { selection, p2selection };
+        },
+
+        move(selection) {
+            const placePlay = document.querySelectorAll(".grid-item");
+            placePlay.forEach(place => place.addEventListener("click", () => {
+                choice = place.dataset.placePosition;
+                gameBoard.board[choice] = selection;
+            }));
         }
     };
-    const symbolSelection = () => {
-        if (document.getElementById("p1").checked) {
-            selection = "O";
-        } else {
-            selection = "X";
-        }
-        return { selection };
-    };
-    const symbolSelectionOther = (selection) => {
-        if (selection === "O") {
-            otherSelection = "X";
-        } else {
-            otherSelection = "O";
-        }
-    };
-    const move = selection => {
-        const placePlay = document.querySelectorAll(".grid-item");
-        placePlay.forEach(place => place.addEventListener("click", () => {
-            choice = place.dataset.placePosition;
-            gameBoard.board[choice] = selection;
-        }));
-    };
-    return { getName, firstturn, symbolSelection, move, symbolSelectionOther }
 }
 
 
 
-function gameFlow() {
+/* function gameFlow() {
     document.getElementById("settings").style.display = "none";
-    const newGame = gameBoard();
+    const newGame = gameBoard.createBoard();
     const playerOne = player("Player One");
     const playerTwo = player("Player Two");
     newGame.createBoard();
     return { newGame, newBoard, playerOne, playerTwo };
-}
+} */
 
+const jim = player('jim');
 
-gameStart.addEventListener("click", gameFlow);
+gameStart.addEventListener("click", function () {
+    document.getElementById("settings").style.display = "none";
+    gameBoard.createBoard;
+});
+
